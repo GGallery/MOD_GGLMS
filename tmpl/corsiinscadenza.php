09f51d76956e33e4c521603dcd53362b1523a5f4
antoniopetruzzella <a.petruzzella71@gmail.com>
2018-03-22 10:29:15 +0100

    implementata la vista corsi in scadenza

--- tmpl/corsiinscadenza.php
+++ tmpl/corsiinscadenza.php
@@ -2,7 +2,7 @@
 
 
 $corsiinscadenza = modgglmsHelper::getCorsiInScadenza();
-echo '<div class="bg-danger" style="color:black; padding: 5px;"> <H3>CORSI IN SCADENZA:</H3>';
+echo '<div class="bg-danger corsiinscadenzabox"> <H3>CORSI IN SCADENZA:</H3>';
 if(count($corsiinscadenza)>0) {
 
     echo '<ul>';
