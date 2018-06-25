365b614660591c98f0accaa1dac802825eb9f06e
antoniopetruzzella <a.petruzzella71@gmail.com>
2018-06-25 17:44:33 +0200

    cruscotto finale

---
+++ tmpl/cruscottoformativo.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+<div class="row" style="border-bottom: 1px solid #0095ad;">
+    <!--    <div class="text-left"><h2>CRUSCOTTO FORMATIVO</h2></div>-->
+
+    <div class="col-md-6" >
+
+        <div class="progress">
+            <div class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+                Il tuo monte ore ESMA 45% Complete
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6" >
+
+        <div class="progress">
+            <div class="progress-bar progress-bar-warning progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+                Il tuo monte ore IVASS 75% Complete
+            </div>
+        </div>
+    </div>
+</div>
