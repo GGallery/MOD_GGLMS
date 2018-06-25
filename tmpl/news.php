2eaee93ca67017fde2e02bd42057fab8ee056a76
antoniopetruzzella <a.petruzzella71@gmail.com>
2018-06-25 14:18:45 +0200

    news

--- tmpl/news.php
+++ tmpl/news.php
@@ -4,7 +4,7 @@ $ultimoArticolo=modgglmsHelper::getUltimoArticolo($unita);
 
 ?>
 <div class="news">
-    <div class="text-center"><h3>CARIGE NEWS</h3></div>
+    <div class="text-center"><h3>CARIGE LEARNING NEWS</h3></div>
     <div class="text-center">
         <h2>
             <a href="index.php?option=com_gglms&view=contenuto&alias=<?php echo $ultimoArticolo->alias; ?>">
